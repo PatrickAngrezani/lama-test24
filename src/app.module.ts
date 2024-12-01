767f2a6a7aa69aeb7848b605b35c7b097e49d93f
PatrickAngrezani <pangrezani@gmail.com>
2024-11-30 23:15:01 -0300

    feat: configure database

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,27 @@ import { AuthModule } from './auth/auth.module';
 import { BalancesModule } from './balances/balances.module';
 import { TransactionsModule } from './transactions/transactions.module';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [AuthModule, BalancesModule, TransactionsModule, UserModule],
+  imports: [
+    AuthModule,
+    BalancesModule,
+    TransactionsModule,
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USERNAME,
+      password: String(process.env.POSTGRES_PASSWORD),
+      database: process.env.POSTGRES_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
